a2a38221b2ca44f5600dd2c36f476b915afef4cb
maiuyb <2950801864@qq.com>
2025-06-10 16:34:26 +0800

    Add files via upload

---
+++ b5b275d6f487455c94c82ec85e144e24/433-2025_jvm_03/src/main/java/com/njuse/seecjvm/classloader/classfileparser/constantpool/info/ClassInfo.java
@@ -0,0 +1,47 @@
+package com.njuse.seecjvm.classloader.classfileparser.constantpool.info;
+
+import com.njuse.seecjvm.classloader.classfileparser.constantpool.ConstantPool;
+import jdk.jfr.Unsigned;
+
+
+public class ClassInfo extends ConstantPoolInfo {
+
+
+    /**
+     * Add some codes here.
+     *
+     * tips:
+     * step1
+     *      ClassInfo need a private field, what is it?
+     *
+     * step2
+     *      You need to add some args in constructor
+     *      and don't forget to set tag
+     *
+     *      super method and super key word will help you
+     */
+
+    //todo attibute of ClassInfo
+
+
+    //todo constructor of ClassInfo
+    public ClassInfo(ConstantPool constantPool, int nameIndex){
+        super(constantPool);
+
+    }
+
+    //todo getClassName
+    public String getClassName() {
+        /**
+         * Add some codes here.
+         * tips: classname is in UTF8Info
+         */
+        return "";
+    }
+
+    @Override
+    public String toString() {
+
+        return getClassName();
+    }
+}
---
+++ b5b275d6f487455c94c82ec85e144e24/433-2025_jvm_03/src/main/java/com/njuse/seecjvm/memory/jclass/Field.java
@@ -0,0 +1,18 @@
+package com.njuse.seecjvm.memory.jclass;
+
+import com.njuse.seecjvm.classloader.classfileparser.FieldInfo;
+import com.njuse.seecjvm.classloader.classfileparser.attribute.CodeAttribute;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Field extends ClassMember {
+    public Field(FieldInfo info, JClass clazz) {
+        //todo initilize the attributes from info
+        /**
+         * tips: refer to constructor of Method
+         */
+
+    }
+}
